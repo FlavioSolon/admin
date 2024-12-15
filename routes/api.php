935425901eb4e47e9e74b51a933ffc0fb1b19306
SolonDev25 <flavio.solon@aspenbrasil.com.br>
2024-12-15 18:05:35 -0400

    test: api about_team

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Models\AboutTeam;
+use App\Http\Resources\About\AboutTeamResource;
+
+Route::get('/about/about-team', function () {
+    return AboutTeamResource::collection(AboutTeam::all());
+});
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
