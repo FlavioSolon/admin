34602cd4e923d303df7c7a82918b1d82eacf9a4e
SolonDev25 <flavio.solon@aspenbrasil.com.br>
2024-12-15 18:23:11 -0400

    feat: api about complet

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,14 @@ use Illuminate\Support\Facades\Route;
 use App\Models\AboutTeam;
 use App\Http\Resources\About\AboutTeamResource;
 
+
+// About
 Route::get('/about/about-team', function () {
     return AboutTeamResource::collection(AboutTeam::all());
 });
+
+
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
