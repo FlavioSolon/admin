91e7d7dfd979758c72a18b1a879355ad8347d1c7
SolonDev25 <flavio.solon@aspenbrasil.com>
2025-02-11 09:37:20 -0400

    refactory: FindUS e StoreLocationResource.php

---
+++ app/Http/Resources/FindUs/StoreLocationResource.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Resources\FindUs;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StoreLocationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'map_url' => $this->map_url,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
